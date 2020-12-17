055036e2cb2caa7d7fb19c1309c8b958d5161a35
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-17 12:58:19 -0300

    'add_formGroup'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { MaterialModule } from './shared/material/material.module';
 import { TopoComponent } from './shared/components/topo/topo.component';
 import { RodapeComponent } from './shared/components/rodape/rodape.component';
 
-import { FilmesModule } from './filmes/filmes.module';
 
 
 @NgModule({
@@ -26,7 +25,7 @@ import { FilmesModule } from './filmes/filmes.module';
     LayoutModule,
     MaterialModule,
     AppRoutingModule,
-    FilmesModule
+   
   ],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
   bootstrap: [AppComponent]
