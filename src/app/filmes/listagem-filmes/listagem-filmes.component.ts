06d451aefeccf5856ba2fb29e8b964ade274de02
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-21 08:51:39 -0300

    filters_form

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
@@ -12,13 +13,41 @@ export class ListagemFilmesComponent implements OnInit {
   
   readonly qtdPagina = 4;
   pagina = 0;
+  texto: string;
+  genero: string
   filmes: Filme[] = [];
-  
+  filtrosListagem: FormGroup;
+  generos: Array<string> 
   
 
-  constructor(private filmeService: FilmesService) { }
+  constructor(private filmeService: FilmesService,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
+        console.log('alteracao valor texto', val);
+    });
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      console.log('alteracao valor genero', val);
+    });
+
+    this.generos = [
+      'Ação',
+      'Romance',
+      'Aventura',
+      'Terror',
+      'Ficção cientifica',
+      'Comédia',
+      'Drama'
+    ];
+
+    
+
     this.listarFilmes();
   }
 
