4f0978ab313d030ec86f9ec8d10f2e376850a063
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-22 15:30:02 -0300

    CRUD

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -21,6 +21,10 @@ export class FilmesService {
      return this.http.post<Filme>(url, filme);
   }
 
+  editar(filme: Filme): Observable<Filme> {
+    return this.http.put<Filme>(url + filme.id, filme);
+  }
+
   listar(config: ConfigPrams): Observable<Filme[]> {
     const ConfigPrams = this.configService.configurarParamentros(config);
     return this.http.get<Filme[]>(url, {params: ConfigPrams});
--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -63,7 +63,12 @@ export class CadastroFilmesComponent implements OnInit {
     }
     
     const filme = this.cadastro.getRawValue() as Filme;
-    this.salvar(filme);
+    if (this.id) {
+      filme.id = this.id;
+      this.editar(filme);
+    }else {
+      this.salvar(filme);
+    }
   }
 
   reiniciarForm(): void{
@@ -126,4 +131,27 @@ export class CadastroFilmesComponent implements OnInit {
       this.dialog.open(AlertaComponent, config);
     });
   }
+  private editar(filme: Filme): void {
+    this.filmeService.editar(filme).subscribe(() => {
+      const config = {
+        data: {
+         descricao: 'Seu registro foi atualizado com sucesso',
+         btnSucesso: 'Ir para a listagem',
+        } as Alerta
+      };
+      const dialogRef = this.dialog.open(AlertaComponent, config);
+      dialogRef.afterClosed().subscribe(() => this.router.navigateByUrl('filmes'));  
+    },
+    () => {
+     const config = {
+       data: {
+        titulo: 'Erro ao editar o Registro!',
+        descricao:'Não conseguimos editar seu registro, favor tentar novamente mais tarde.',
+        corBtnSucesso: 'warn',
+        btnSucesso: 'Fechar',
+       } as Alerta
+    };
+     this.dialog.open(AlertaComponent, config);
+   });
+ }
 }
